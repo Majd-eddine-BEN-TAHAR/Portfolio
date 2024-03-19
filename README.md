83897c0e0c3f04dac2ee24731106be2ab7271e09
= <majdeddinebentahar@gmail.com>
2024-03-19 10:06:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Portfolio
+## Majd Eddine BEN TAHAR | FRONT-END DEVELOPER
+This is my Personal Portfolio website, for live preview visit : [Majd Portfolio](https://majd-eddine-ben-tahar.github.io/Portfolio/).
---
+++ js/main.js
@@ -0,0 +1,55 @@
+var string = `I'm a passionate and self-taught Full Stack JS Developer with experience in ReactJS, NodeJS, and Express. I'm also proficient in PHP and Symfony. As a dedicated learner, I'm constantly looking for new challenges to improve my skills and knowledge.
+\n Always striving for excellence, I aim to apply my abilities and knowledge to achieve company goals and positively contribute to an organization's future growth. If you're looking for a talented Full Stack Web Developer with a passion for web development, please don't hesitate to get in touch!`;
+var str = string.split("");
+var el = document.getElementById("about-p");
+(function animate() {
+  str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
+  var running = setTimeout(animate, 20);
+})();
+
+document.querySelector(".menu-toggler").addEventListener("click", function () {
+  this.classList.toggle("open");
+  document.querySelector(".top-nav").classList.toggle("open");
+});
+
+document.querySelectorAll(".top-nav .nav-link").forEach(function (element) {
+  element.addEventListener("click", function () {
+    document.querySelector(".menu-toggler").classList.remove("open");
+    document.querySelector(".top-nav").classList.remove("open");
+  });
+});
+
+document.querySelectorAll('nav a[href*="#"]').forEach(function (anchor) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+document.getElementById("up").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+AOS.init({
+  easing: "ease",
+  duration: 1800,
+  once: true,
+});
+function toggleTooltip() {
+  var tooltip = document.getElementById("emailTooltip");
+  tooltip.style.display = tooltip.style.display === "none" ? "block" : "none";
+}
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(
+    function () {
+      alert("Email copied to clipboard!");
+    },
+    function (err) {
+      console.error("Error copying to clipboard", err);
+    }
+  );
+}
